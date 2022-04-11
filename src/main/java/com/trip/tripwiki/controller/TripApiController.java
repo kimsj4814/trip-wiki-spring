a2f0498672cb1dd60a72d2b5a651ef8c9909932f
kimsj4814@naver.com <user.email>
2022-04-11 17:16:39 +0900

    API 페이징 처리

--- src/main/java/com/trip/tripwiki/controller/TripApiController.java
+++ src/main/java/com/trip/tripwiki/controller/TripApiController.java
@@ -6,10 +6,14 @@ import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
 import java.net.HttpURLConnection;
 
 import org.json.JSONObject;
 import org.json.XML;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,15 +22,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TripApiController {
-
+	
+	private static final Logger logger = LoggerFactory.getLogger(TripApiController.class);
+	
 	@Value("${my.apikey}")
 	private String API_KEY;
 	
 	@GetMapping("/apitest")
-	public String callApiWithXml(String keyword) {
+	public Map<String,Object> callApiWithXml(String keyword, int page) {
 		StringBuffer result = new StringBuffer();
 		String jsonPrintString = null;
-		keyword = "서울";
+		//keyword = "서울";
 		String encodeResult = null;
 		try {
 			encodeResult = URLEncoder.encode(keyword,"UTF-8");
@@ -38,8 +44,8 @@ public class TripApiController {
 				+ "service/rest/KorService/searchKeyword?"
 				+ "serviceKey="+API_KEY
 				+"&MobileApp=AppTest&MobileOS=ETC&"
-				+ "pageNo=1&numOfRows=8&listYN=Y&arrange=A&contentTypeId=12&keyword=" +encodeResult;
-
+				+ "pageNo="+page+"&numOfRows=8&listYN=Y&arrange=A&contentTypeId=12&keyword=" +encodeResult;
+																										//+"&areaCode=1;"
 				try {
 					URL url = new URL(apiUrl);
 					HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
@@ -51,6 +57,7 @@ public class TripApiController {
 		            while((returnLine = bufferedReader.readLine()) != null) {
 		                result.append(returnLine);
 		            }
+		            logger.info(result.toString());
 		            
 		            JSONObject jsonObject = XML.toJSONObject(result.toString());
 		            jsonPrintString = jsonObject.toString();
@@ -60,8 +67,29 @@ public class TripApiController {
 					e.printStackTrace();
 				}
 				
-				return jsonPrintString;
+				int listcount = 96;
+				int limit = 8;
+				int maxpage = (listcount + limit - 1) / limit;
+
+				if(page>maxpage) {
+					page=maxpage;
+				}
 				
+				int startpage = ((page - 1) / 10) * 10 + 1;
+				int endpage = startpage + 10 - 1;
+
+				if (endpage > maxpage)
+					endpage = maxpage;
+				
+				Map<String, Object> map = new HashMap<String,Object>();
+		        map.put("page", page);
+		        map.put("maxpage", maxpage);
+		        map.put("startpage", startpage);
+		        map.put("endpage", endpage);
+		        map.put("listcount", listcount);
+		        map.put("boardlist", jsonPrintString);
+		        map.put("limit", limit);
+		        return map;
 				
 	}
 }
