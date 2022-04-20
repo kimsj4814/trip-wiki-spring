556ab0e7e5fc95425b15b134152aae052eca2c53
wjrdjs005 <wjrdjs005@naver.com>
2022-04-20 19:02:31 +0900

    commits

---
+++ src/main/java/com/trip/tripwiki/controller/UserVueController.java
@@ -0,0 +1,30 @@
+package com.trip.tripwiki.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class UserVueController {
+
+	@GetMapping("/trip/login")
+	public String LoginView() {
+		return "index.html";
+	}
+	@GetMapping("/trip/Idsearch")
+	public String LoginFinder() {
+		return "index.html";
+	}
+	@GetMapping("/trip/PwdSearch")
+	public String PwdSearch() {
+		return "index.html";
+	}
+	@GetMapping("/trip/join")
+	public String Join() {
+		return "index.html";
+	}
+	@GetMapping("/trip/auth")
+	public String mailAuth() {
+		return "index.html";	
+	}
+}
